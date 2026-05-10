f2db14a0eed435f19dae02d4f71a75e015668719
Dr. Pascal Pollet <pascal.pollet@mailbox.org>
2026-05-11 00:35:46 +0200

    Update Angular to v19, migrate sass to @use instead @import

--- src/app/utils/fix-amp.pipe.ts
+++ src/app/utils/fix-amp.pipe.ts
@@ -1,7 +1,9 @@
 import { Pipe, PipeTransform } from '@angular/core';
 
 @Pipe({
-  name: 'fixAmp'
+  name: 'fixAmp',
+  // eslint-disable-next-line
+  standalone: false
 })
 export class FixAmpPipe implements PipeTransform {
   transform(value: string, ...args: Array<unknown>): string {
